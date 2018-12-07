065530e92f5e4e7e46c00b79733d68451172162e
melih <melihyayla@gmail.com>
2018-12-07 17:45:14 +0300

    Show all marker on map

--- app/src/main/java/com/servet/tracklocation/MapsActivity.java
+++ app/src/main/java/com/servet/tracklocation/MapsActivity.java
@@ -5,6 +5,7 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,11 +13,18 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.servet.tracklocation.Helper.DatabaseHelper;
+
+import java.nio.DoubleBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     String address = "";
+    DatabaseHelper databaseHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +33,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        databaseHelper = new DatabaseHelper(this.getApplicationContext());
+
+
+
     }
+
 
 
     /**
@@ -62,6 +76,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if(title!= "Your Location"){
             mMap.addMarker(new MarkerOptions().position(userLocation).title(title));
         }
+
+        ArrayList<HashMap<String,String>> locationList = databaseHelper.activeLocation();
+        ArrayList<String> locations = new ArrayList<>();
+
+        for (int i = 0; i < locationList.size(); i++) {
+            locations.add(String.valueOf(locationList.get(i).get("id")));
+        }
+
+        ArrayList<String> mLocation = new ArrayList<>();
+        //HashMap<String,String> mLocation = new HashMap<>();
+
+        for(int i=0; i<locationList.size(); i++){
+            String str = databaseHelper.locationDetail(Integer.parseInt(locations.get(i))).get("latitude")
+                    + "-" + databaseHelper.locationDetail(Integer.parseInt(locations.get(i))).get("latitude")+ "-" +
+                    databaseHelper.locationDetail(Integer.parseInt(locations.get(i))).get("location_title");
+
+            if(!mLocation.contains(str))
+            mLocation.add(str);
+
+
+
+        }
+
+
+        for (int i = 0; i < mLocation.size(); i++) {
+
+
+            double latitude = Double.parseDouble(mLocation.get(i).split("-")[0]);
+            double longitude = Double.parseDouble(mLocation.get(i).split("-")[1]);
+            String mTitle = mLocation.get(i).split("-")[2];
+
+            LatLng latLng = new LatLng(latitude,longitude);
+
+            mMap.addMarker(new MarkerOptions().position(latLng).title(mTitle));
+
+        }
+
+
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,18));
     }
+
 }
